b4e3fe732d64402e62926db6aa98f093f37a2f9e
g9inlife-blip <g9inlife@gmail.com>
2026-09-01 15:54:43 +0900

    fix: support classic browser script loading

--- work/src/battle/Character.js
+++ work/src/battle/Character.js
@@ -1,27 +1,11 @@
-export class Character {
-  constructor({ id, name, hp, attack, defense, speed = 10, team = 'player' }) {
-    this.id = id;
-    this.name = name;
-    this.maxHp = hp;
-    this.hp = hp;
-    this.attack = attack;
-    this.defense = defense;
-    this.speed = speed;
-    this.team = team;
+class Character {
+  constructor({ id, name, hp, attack, defense, speed = 10, team }) {
+    this.id = id; this.name = name; this.maxHp = hp; this.hp = hp;
+    this.attack = attack; this.defense = defense; this.speed = speed; this.team = team;
     this.stats = { damageDealt: 0, damageTaken: 0, attacks: 0, hits: 0, criticals: 0, misses: 0, kills: 0 };
   }
-
   get alive() { return this.hp > 0; }
 }
-
-export function cloneCharacter(character) {
-  return new Character({
-    id: character.id,
-    name: character.name,
-    hp: character.maxHp,
-    attack: character.attack,
-    defense: character.defense,
-    speed: character.speed,
-    team: character.team
-  });
-}
+function cloneCharacter(character) { return new Character({ id: character.id, name: character.name, hp: character.maxHp, attack: character.attack, defense: character.defense, speed: character.speed, team: character.team }); }
+window.Character = Character;
+window.cloneCharacter = cloneCharacter;
