aa0950cbd0de89fee3911ca300fce3662f203676
g9inlife-blip <g9inlife@gmail.com>
2026-09-01 15:43:47 +0900

    feat: add battle character model

---
+++ work/src/battle/Character.js
@@ -0,0 +1,25 @@
+export class Character {
+  constructor({ id, name, hp, attack, defense, speed = 10 }) {
+    this.id = id;
+    this.name = name;
+    this.maxHp = hp;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.speed = speed;
+    this.stats = { damageDealt: 0, damageTaken: 0, attacks: 0, hits: 0, criticals: 0, misses: 0, kills: 0 };
+  }
+
+  get alive() { return this.hp > 0; }
+}
+
+export function cloneCharacter(character) {
+  return new Character({
+    id: character.id,
+    name: character.name,
+    hp: character.maxHp,
+    attack: character.attack,
+    defense: character.defense,
+    speed: character.speed
+  });
+}
