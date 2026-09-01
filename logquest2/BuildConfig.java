9015de558282fdd635437f2b6824107a198cc599
g9inlife-blip <g9inlife@gmail.com>
2026-09-01 13:44:27 +0900

    Add files via upload

---
+++ logquest2/BuildConfig.java
@@ -0,0 +1,11 @@
+package com.shirobakama.logquest2;
+
+/* loaded from: D:\Android_hobby\작업중\com.shirobakama.logquest2-1.3.13\classes.dex */
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.shirobakama.logquest2";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1610313;
+    public static final String VERSION_NAME = "1.3.13";
+}
