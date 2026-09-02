bd21440c883622d2acb2654df49c50634e408640
g9inlife-blip <g9inlife@gmail.com>
2026-09-02 13:27:54 +0900

    feat: add predefined region configuration

---
+++ work/data/region-config.js
@@ -0,0 +1,15 @@
+// Predefined world regions. Dungeon names are resolved against the XML-derived dungeon registry.
+window.REGION_CONFIG = {
+  'region-forest-frontier': {
+    id:'region-forest-frontier',
+    name:'엘드리아 숲지대',
+    description:'초기 모험가들이 탐험하는 숲과 고블린 세력권.',
+    dungeons:['고대의 숲','고블린 통로','고블린 동굴']
+  },
+  'region-ancient-ruins': {
+    id:'region-ancient-ruins',
+    name:'고대 유적지대',
+    description:'오래된 문명의 흔적과 강력한 수호자가 남아 있는 유적 권역.',
+    dungeons:['고대 유적']
+  }
+};
