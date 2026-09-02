7701dce5f3883793dd8168f56eb300be2e29fe43
g9inlife-blip <g9inlife@gmail.com>
2026-09-02 11:00:08 +0900

    feat: add healing spring exploration event

---
+++ work/src/world/DungeonEvents.js
@@ -0,0 +1,21 @@
+(() => {
+  const originalStep = DungeonRun.prototype.step;
+  DungeonRun.prototype.step = function(...args) {
+    if (this.active && this.floor > 0 && this.rng.int(1,100) <= 2) {
+      for (const c of this.party || []) {
+        if (!c) continue;
+        c.hp = Math.min(c.maxHp, c.hp + Math.ceil(c.maxHp * 0.30));
+        if (Number.isFinite(c.maxMp)) c.mp = Math.min(c.maxMp, (Number(c.mp)||0) + Math.ceil(c.maxMp * 0.30));
+      }
+      this.emit('HEALING_SPRING', {
+        hpPercent:30, mpPercent:30,
+        characters:(this.party||[]).map(c=>({id:c.id,name:c.name,hp:c.hp,maxHp:c.maxHp,mp:c.mp,maxMp:c.maxMp}))
+      });
+      if (this.floor === this.dungeon.floors) {
+        this.complete=true; this.active=false; this.emit('RUN_COMPLETE',{floor:this.floor});
+      } else this.nextFloor();
+      return {done:this.complete,battle:null};
+    }
+    return originalStep.apply(this,args);
+  };
+})();
