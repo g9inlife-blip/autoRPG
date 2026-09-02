8edc90833fd27923555d024ab30897003aa923e8
g9inlife-blip <g9inlife@gmail.com>
2026-09-03 07:56:12 +0900

    fix: remove unstable auto progression loader

--- work/src/ui/RegionPanel.js
+++ work/src/ui/RegionPanel.js
@@ -81,11 +81,4 @@
     wrapped._regionNavigationLock=true;window.render=wrapped;
   }
   syncControls();
-
-  if(!document.querySelector('script[data-dungeon-progress-controller]')){
-    const script=document.createElement('script');
-    script.src='src/ui/DungeonProgressController.js?v=20260903-1';
-    script.dataset.dungeonProgressController='1';
-    document.body.appendChild(script);
-  }
 })();
